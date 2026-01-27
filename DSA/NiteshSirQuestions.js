97d2327e284becc1ee97eca481b250f2cd242f80
Prisans <124236508+Prisans@users.noreply.github.com>
2026-01-27 13:12:07 +0530

    second-largest

--- DSA/NiteshSirQuestions.js
+++ DSA/NiteshSirQuestions.js
@@ -49,23 +49,69 @@
 
 // Two pointer approach ->
 
-const removeDuplicate = (arr)=>{
-    let left = 0
+// const removeDuplicate = (arr)=>{
+//     let left = 0
     
-    for(let i=1; i<arr.length; i++){
-        if(arr[left] != arr[i]){
-            left++
-            arr[left] = arr[i]
-        }
-    }
+//     for(let i=1; i<arr.length; i++){
+//         if(arr[left] != arr[i]){
+//             left++
+//             arr[left] = arr[i]
+//         }
+//     }
 
-    return arr.slice(0,left+1)
-}
+//     return arr.slice(0,left+1)
+// }
 
 
 // console.log(removeDuplicate([1, 2, 2, 3, 4, 4, 5])) - All test cases passed
 
 // console.log(removeDuplicate([1, 2, 2, 3, 4, 4, 5])) - all test cases run
+
+// Q-------------------->Find second largest number
+
+// Input: [10, 5, 20, 8]
+
+// Output: 10
+
+// Sol  
+
+// const findSecondLargest = (arr)=>{
+//     let largest = -Infinity
+//     let secondLargest = -Infinity
+
+//     for(let i=0; i<arr.length; i++){
+//         let num = arr[i]
+
+//         if(num>largest){
+//             secondLargest = largest
+//             largest = num
+//         }else if(num<largest && num>secondLargest){
+//             secondLargest = num
+//         }
+//     }
+
+//     return secondLargest
+    
+// }
+
+// let arr  = [10, 5, 20, 8]
+
+// console.log(findSecondLargest(arr))
+
+
+// Q----------------------->Most frequent element in array
+
+// Input: [1, 2, 2, 3, 3, 3, 4]
+
+// Output: 3
+
+const frequentNum = (arr)=>{
+    let demo = new Set()
+    let 
+
+
+}
+
 
 
 
